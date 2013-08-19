e4f68a71b5d68bc9d82e120c60ed72ce68a64823
Joel Amálio <joel.amalio@gmail.com>
2013-08-19 15:32:42 -0300

    Mudança do contexto da aplicação de "web" para "backfeed".

--- web/src/main/webapp/static/js/app/apresentacao.js
+++ web/src/main/webapp/static/js/app/apresentacao.js
@@ -2,7 +2,7 @@ require(["jquery", "ko"], function($, ko) {
     var model = {
         apresentacoes: ko.observableArray(),
         buscarApresentacoes: function() {
-            $.getJSON('/web/apresentacao/lista.json', this.apresentacoes);
+            $.getJSON('/backfeed/apresentacao/lista.json', this.apresentacoes);
         },
         init: function() {
             ko.applyBindings(this);
